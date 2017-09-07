cb70a57dff44422e6f62886553c22ad4ddd0d228
SuvStreet <suvstreet@gmail.com>
2017-09-07 21:38:35 +0300

    Update Additional_Task1.cs

--- HomeWork/Day3/Additional_Task1.cs
+++ HomeWork/Day3/Additional_Task1.cs
@@ -10,38 +10,23 @@ namespace Day3
             System.Int32[] arr = new int[10];
             Random rnd = new Random();
 
-            for (int i = 0; i < arr.Length; i++) {
+            for (int i = 0; i < arr.Length; i++)
+            {
                 arr[i] = rnd.Next(-10, 10);
             }
 
-            Console.WriteLine(String.Join(" ", arr));
-            Console.WriteLine();
-
-            int x = 1;
-            int y = 1;
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = 0; j < arr.Length; j++)
-                {
-                    if (i != j)
-                    {
-                        if (x < arr[i] * arr[j]) { x = arr[i] * arr[j]; };
-                    }
-                    else continue;
-                }
-            }
-          
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (y < arr[i] * x) { y = x * arr[i]; };
-            }
-
-            Console.WriteLine(y);
-            Console.WriteLine();
             Array.Sort(arr);
+
             Console.WriteLine(String.Join(" ", arr));
             Console.WriteLine();
+
+            int multiplication_1 = arr[0] * arr[1] * arr[arr.Length - 1];
+            int multiplication_2 = arr[arr.Length - 3] * arr[arr.Length - 2] *arr[arr.Length - 1];
+
+            int multiplication_3 = (multiplication_1 > multiplication_2) ? multiplication_1 : multiplication_2;
+
+            Console.WriteLine(multiplication_3);
+  
         }
     }
 }
