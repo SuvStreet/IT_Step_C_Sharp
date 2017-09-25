1f17fa852868c0bd46ed85afcb118cba7fa2edb4
SuvStreet <suvstreet@gmail.com>
2017-09-25 19:01:03 +0300

    Create Door.cs

---
+++ HomeWork/Day11/HouseConstructionLib/PartsHouse/Door.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseConstructionLib
+{
+    // дверь
+    public class Door : Part
+    {
+        public Door() : base(50) { }
+    }
+}
