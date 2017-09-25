846f633a4568be5643f45ec19299e4486ae0b43a
SuvStreet <suvstreet@gmail.com>
2017-09-25 19:01:17 +0300

    Create Roof.cs

---
+++ HomeWork/Day11/HouseConstructionLib/PartsHouse/Roof.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseConstructionLib
+{
+    // Крыша
+    public class Roof : Part
+    {
+        public Roof() : base(150) { }
+    }
+}
