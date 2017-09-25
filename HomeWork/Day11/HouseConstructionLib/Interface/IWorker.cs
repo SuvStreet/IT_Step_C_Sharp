8871cc56976fd375fe55d33e173bd0c5e7d4d724
SuvStreet <suvstreet@gmail.com>
2017-09-25 18:59:58 +0300

    Create IWorker.cs

---
+++ HomeWork/Day11/HouseConstructionLib/Interface/IWorker.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseConstructionLib
+{
+    public interface IWorker
+    {
+        // работа рабочих и бригадиров
+        void Work();
+    }
+}
